ec50faad7d3977ef2cd0c33777fb16b5169ae197
OrionBS <work@orionbs.fr>
2021-10-22 09:33:06 +0200

    postConstruct

--- Poseidon_SpringAPI/src/main/java/fr/orionbs/services/impl/AdminInitImpl.java
+++ Poseidon_SpringAPI/src/main/java/fr/orionbs/services/impl/AdminInitImpl.java
@@ -3,9 +3,15 @@ package fr.orionbs.services.impl;
 import fr.orionbs.models.User;
 import fr.orionbs.repositories.UserRepository;
 import fr.orionbs.services.AdminInit;
+import fr.orionbs.services.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
+@Slf4j
+@Service
 public class AdminInitImpl implements AdminInit {
 
     private final UserRepository userRepository;
@@ -19,8 +25,11 @@ public class AdminInitImpl implements AdminInit {
     public void postContruct() {
         User isPresent = userRepository.findByUsername("admin");
         if (isPresent == null) {
-            User admin = new User(null, "admin", "adminPassword", "Admin", "ADMIN");
+            log.info("Admin added");
+            User admin = new User(null, "admin", new BCryptPasswordEncoder().encode("adminPassword"), "Admin", "ADMIN");
             userRepository.save(admin);
+        } else {
+            log.info("Admin already present.");
         }
     }
 }
